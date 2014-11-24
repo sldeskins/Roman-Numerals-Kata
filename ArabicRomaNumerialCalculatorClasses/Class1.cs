d671fc8c4696202b049d564c2631fc2a58f333f7
sldeskins <sldeskins@gmail.com>
2014-11-23 21:53:23 -0500

    The symbols 'I', 'X', 'C', and 'M' can be repeated at most 3 times in a row and test

---
+++ ArabicRomaNumerialCalculatorClasses/Class1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+//http://agilekatas.co.uk/katas/romannumerals-kata.html
+namespace ArabicRomaNumerialCalculatorClasses
+    {
+
+    public class Class1
+        {
+        protected String message = null;
+        public List<char> VALID_ROMMAN_LETTERS = null;
+        protected List<char> VALID_ROMMAN_LETTERS_LOWERCASE = null;
+
+        public Class1 ()
+            {
+
+            VALID_ROMMAN_LETTERS = new List<char>() { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+            VALID_ROMMAN_LETTERS_LOWERCASE = new List<char>() { 'i', 'v', 'x', 'l', 'c', 'd', 'm' };
+            }
+
+        public bool RomanNumeralStringIsValid ( string testRomanCharacterString )
+            {
+
+            bool isValid = false;
+
+            bool isValidRomanNumeralCharacters = true;
+            bool hasLowercaseRomanNumerals = false;
+            string hasLowercaseRomanNumeralsMeaasge = "Lower case roman numerals are not valid.";
+
+
+            bool hasMoreThanThreeRomanNumeralsRepeats = false;
+            string hasMoreThanThreeRomanNumeralsRepeatsMessage = "'I', 'X', 'C', and 'M' can be repeated at most 3 times in a row.";
+
+
+            message = "";
+            List<String> InValidCharacters = new List<string>();
+
+            List<char> testRomanCharList = testRomanCharacterString.ToList();
+            testRomanCharList.ForEach(delegate( char romanChar )
+            {
+                if (!VALID_ROMMAN_LETTERS.Contains(romanChar))
+                    {
+                    isValidRomanNumeralCharacters = false;
+                    InValidCharacters.Add(romanChar.ToString());
+
+                    if (VALID_ROMMAN_LETTERS_LOWERCASE.Contains(romanChar))
+                        {
+                        hasLowercaseRomanNumerals = true;
+                        }
+                    }
+
+            });
+
+            string pattern = @"([I]{4,}|[X]{4,}|[C]{4,}|[M]{4,})+";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(testRomanCharacterString))
+                {
+                hasMoreThanThreeRomanNumeralsRepeats = true;
+                }
+
+
+            isValid = isValidRomanNumeralCharacters && !hasMoreThanThreeRomanNumeralsRepeats;
+            if (!isValid)
+                {
+                List<string> messageList = new List<string>();
+                if (!isValidRomanNumeralCharacters)
+                    {
+                    messageList.Add(String.Join(null, InValidCharacters) + " is not a valid roman numeral.");
+                    }
+                if (hasLowercaseRomanNumerals)
+                    {
+                    messageList.Add(hasLowercaseRomanNumeralsMeaasge);
+                    }
+                if (hasMoreThanThreeRomanNumeralsRepeats)
+                    {
+                    messageList.Add(hasMoreThanThreeRomanNumeralsRepeatsMessage);
+                    }
+
+                message = string.Join(" ", messageList);
+
+                }
+            else
+                {
+                message = "ok";
+                }
+            return isValid;
+            }
+
+        public string getCommandMessage ()
+            {
+            return message;
+            }
+        }
+
+    }
---
+++ UnitTestProject1/BeiningTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArabicRomaNumerialCalculatorClasses;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+    {
+    [TestClass]
+    public class UnitTest1
+        {
+
+
+        [TestMethod]
+        public void Test_RomanNumeralStringIsValid ()
+            {
+            var testDataCollection = new[] { 
+//invalid inputs
+new { testInput = "A", expectedIsValid = false, expectedMessage = "A is not a valid roman numeral." },
+new { testInput = "a", expectedIsValid = false, expectedMessage = "a is not a valid roman numeral." },
+               
+//valid single inputs         
+//The Romans wrote their numbers using letters; specifically the letters "I, V, X, L, C, D, and M."
+new { testInput = "I", expectedIsValid = true, expectedMessage = "ok" },
+new { testInput = "V", expectedIsValid = true, expectedMessage = "ok" },
+new { testInput = "X", expectedIsValid = true, expectedMessage = "ok" },
+new { testInput = "L", expectedIsValid = true, expectedMessage = "ok" },
+new { testInput = "C", expectedIsValid = true, expectedMessage = "ok" },
+new { testInput = "D", expectedIsValid = true, expectedMessage = "ok" },
+new { testInput = "M", expectedIsValid = true, expectedMessage = "ok" },
+                      
+new { testInput = "VI", expectedIsValid = true, expectedMessage = "ok" },
+              
+//invalid single inputs            
+new { testInput = "i", expectedIsValid = false, expectedMessage = "i is not a valid roman numeral. Lower case roman numerals are not valid." },
+new { testInput = "v", expectedIsValid = false, expectedMessage = "v is not a valid roman numeral. Lower case roman numerals are not valid." },
+new { testInput = "x", expectedIsValid = false, expectedMessage = "x is not a valid roman numeral. Lower case roman numerals are not valid." },
+new { testInput = "l", expectedIsValid = false, expectedMessage = "l is not a valid roman numeral. Lower case roman numerals are not valid." },
+new { testInput = "c", expectedIsValid = false, expectedMessage = "c is not a valid roman numeral. Lower case roman numerals are not valid." },
+new { testInput = "d", expectedIsValid = false, expectedMessage = "d is not a valid roman numeral. Lower case roman numerals are not valid." },
+new { testInput = "m", expectedIsValid = false, expectedMessage = "m is not a valid roman numeral. Lower case roman numerals are not valid." },
+
+//invalid can not be repeated more than three time     
+//The symbols 'I', 'X', 'C', and 'M' can be repeated at most 3 times in a row.
+new { testInput = "II", expectedIsValid = true, expectedMessage = "ok" },
+new { testInput = "III", expectedIsValid = true, expectedMessage = "ok" },
+new { testInput = "IIII", expectedIsValid = false, expectedMessage = "'I', 'X', 'C', and 'M' can be repeated at most 3 times in a row."},
+new { testInput = "IIIXI", expectedIsValid = true, expectedMessage = "ok" },
+new { testInput = "IIIIIIII", expectedIsValid = false, expectedMessage = "'I', 'X', 'C', and 'M' can be repeated at most 3 times in a row."},
+new { testInput = "MMMMM", expectedIsValid = false, expectedMessage = "'I', 'X', 'C', and 'M' can be repeated at most 3 times in a row."},
+new { testInput = "CCCCCCCC", expectedIsValid = false, expectedMessage = "'I', 'X', 'C', and 'M' can be repeated at most 3 times in a row."},
+new { testInput = "XXXXXX", expectedIsValid = false, expectedMessage = "'I', 'X', 'C', and 'M' can be repeated at most 3 times in a row."},
+
+            };
+
+
+            Class1 testClass = new Class1();
+
+            foreach (var testData in testDataCollection)
+                {
+
+                bool result = testClass.RomanNumeralStringIsValid(testData.testInput);
+                Assert.AreEqual(testData.expectedIsValid, result, testData.testInput);
+
+                string message = testClass.getCommandMessage();
+                Assert.AreEqual(testData.expectedMessage, message, testData.testInput);
+                }
+            }
+
+        }
+    }
